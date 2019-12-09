1dd4ed932bfd17ae1d088240690a8d1ed0763856
TheWumboMan <57309325+TheWumboMan@users.noreply.github.com>
2019-12-08 16:55:13 -0800

    wicked cool item view

--- JohnsGarage/src/main/java/graphics/GUIItemView.java
+++ JohnsGarage/src/main/java/graphics/GUIItemView.java
@@ -1,18 +1,24 @@
 package graphics;
 
+import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Scanner;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
@@ -36,13 +42,12 @@ private static final long serialVersionUID = 426473126064216924L;
 	
 	private Path itemPath;
 	
-	public GUIItemView(File theFile, File itemInfo) {
+	public GUIItemView(final File theFile, File itemInfo) {
 		itemName = theFile.getName();
 		int i = itemName.toString().lastIndexOf('.');
 		String extension = "";
 		if (i > 0) extension = itemName.toString().substring(i);
 		itemName = itemName.replace(extension, "");
-		System.out.println("fj: " + extension);
 		
 		setTitle(theFile.getName());
 		setBounds(200, 200, 600, 400);
@@ -53,34 +58,63 @@ private static final long serialVersionUID = 426473126064216924L;
 		setVisible(true);	
 		
 		JLabel title = new JLabel(itemName);
-		title.setFont(new Font("Tahoma", Font.BOLD, 32));
+		title.setFont(new Font("Tahoma", Font.BOLD, 24));
 		setConstraints(0,0,1,1,0.8,0.05);
 		add(title, constraints);
+
 		JTextArea desc = new JTextArea(getDesc(itemInfo));
 		desc.setEditable(false);
-		desc.setFont(new Font("Tahoma", Font.BOLD, 32));
-		setConstraints(0,1,1,1,0.8,0.05);
+		desc.setFont(new Font("Tahoma", Font.BOLD, 24));
+		setConstraints(0,1,2,1,.3,0.95);
 		add(desc, constraints);
+		
+		JButton file = new JButton("Open " + theFile.getName().toString());
+		file.setFont(new Font("Tahoma", Font.BOLD, 24));
+		setConstraints(0,2,1,2,0.2,.05);
+		add(file, constraints);
+		file.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				try
+				{
+					Desktop.getDesktop().open(theFile);
+				}
+				catch (IOException e1)
+				{
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		JButton close = new JButton("Close");
+		close.setFont(new Font("Tahoma", Font.BOLD, 24));
+		setConstraints(1,2,1,1,0.2,.05);
+		add(close, constraints);
+		close.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				dispose();
+			}
+		});
 	}
 	
 	private String getDesc(File itemInfo) {
 		try {
 			Scanner scanner = new Scanner(itemInfo);
 			String tempItemName = "Item Name: " + itemName;
-			System.out.println(tempItemName);
 			while(scanner.hasNextLine()) {
 				if(scanner.nextLine().equals(tempItemName)) {
 					itemDesc = scanner.nextLine();
 					break;
 				}
-				System.out.println("1");
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		System.out.println(itemDesc);
-		//itemDesc.replace("Item Description: ", "");
-		return itemDesc;
+		int i = itemDesc.toString().indexOf(':');
+		return itemDesc.substring(i+1);
 	}
 
 	private void setConstraints(int x, int y, int w, int h, double wx, double wy)
