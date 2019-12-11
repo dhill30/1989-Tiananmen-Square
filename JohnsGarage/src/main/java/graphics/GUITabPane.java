17434b518974058496485c0f2ece97a75d924af8
dhill30 <56419223+dhill30@users.noreply.github.com>
2019-12-10 20:55:24 -0800

    javadoc stuff

--- JohnsGarage/src/main/java/graphics/GUITabPane.java
+++ JohnsGarage/src/main/java/graphics/GUITabPane.java
@@ -1,3 +1,8 @@
+/**
+ * The panel for displaying the user's Tabs. Uses a JScrollPane containing a JList.
+ * Last Edited: 12/4/2019
+ * @author Dylan
+ */
 package graphics;
 
 import java.awt.BorderLayout;
@@ -28,7 +33,9 @@ public class GUITabPane extends JPanel
 	private JScrollPane scrollPane;
 	
 	/**
-	 * TODO
+	 * Creates a JScrollPane of the user's Tabs.
+	 * Last Edited: 12/4/2019
+	 * @author Dylan
 	 * @param fileTree
 	 * @param pm
 	 */
@@ -54,8 +61,10 @@ public class GUITabPane extends JPanel
 	}
 	
 	/**
-	 * TODO
-	 * @return
+	 * Loads a JList of the user's Tabs
+	 * Last Edited: 12/4/2019
+	 * @author Dylan
+	 * @return JList of the user's Tabs
 	 */
 	public JList loadTabs()
 	{
@@ -77,8 +86,10 @@ public class GUITabPane extends JPanel
 	}
 	
 	/**
-	 * TODO
-	 * @return
+	 * Returns the currently selected Tab
+	 * Last Edited: 12/4/2019
+	 * @author Dylan
+	 * @return currently selected Tab
 	 */
 	public Tab getSelected()
 	{
@@ -86,7 +97,9 @@ public class GUITabPane extends JPanel
 	}
 	
 	/**
-	 * TODO
+	 * Refreshes the Tab pane to display any changes
+	 * Last Edited: 12/4/2019
+	 * @author Dylan
 	 */
 	public void refresh()
 	{
