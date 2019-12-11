7c3e1d2dc0ef8f635e1d3c50724fcbd8c4a6c809
TheWumboMan <57309325+TheWumboMan@users.noreply.github.com>
2019-12-10 20:27:57 -0800

    file tree tests

--- JohnsGarage/src/test/java/JUnit/FIleTreeTests.java
+++ JohnsGarage/src/test/java/JUnit/FIleTreeTests.java
@@ -1,3 +1,7 @@
+/**
+ * Unit tests for the file tree
+ * @author James
+ */
 package JUnit;
 
 import static org.junit.Assert.*;
@@ -20,11 +24,16 @@ public class FIleTreeTests {
 	Path ROOTPATH = Paths.get(DEFAULTPATH.toString(), "//testdata");
 	
 	FileTree testTree = new FileTree(true);
-
 	
-	@Test
+	@Test 
 	public void testSetUp() {	
 		assertTrue("root folder doesn't exist", Files.exists(ROOTPATH));
+	}
+	
+	@Test
+	public void testGetTabs() {
+		System.out.println( testTree.getTabs().toString());
+		assertEquals("[Projects, testGFile]", testTree.getTabs().toString());
 	}
 
 	@Test
@@ -50,6 +59,43 @@ public class FIleTreeTests {
 		Item testItem = testTree.newItem("testitem.info", tempFile, testProject);
 		Path temp = Paths.get(ROOTPATH.toString(), "//testTab2//testProject1//testitem.info");
 		assertTrue("new item doesn't exist", Files.exists(temp));
-	}s
+	}
+	
+	@Test
+	public void testDelete() {
+		Tab testDelete = testTree.newTab("testDelete");
+		Project testProject = testTree.newProject("testProject", testDelete);
+		Path temp = Paths.get(ROOTPATH.toString(), "//testDelete//testProject//");
+		testTree.delete(testProject, testDelete);
+		assertTrue("new project doesn't exist", !Files.exists(temp));
+	}
 
+	@Test
+	public void testImportitem() {
+		Path tempFile = Paths.get(ROOTPATH.toString(), "//config.info");
+		Tab testTab3 = testTree.newTab("testTab3");
+		Project testProject = testTree.newProject("testProject", testTab3);
+		testTree.importItem(tempFile, "config.info", testProject);
+		Path temp = Paths.get(ROOTPATH.toString(), "//testTab3//testProject//config.info");
+		assertTrue("new item doesn't exist", Files.exists(temp));
+	}
+	
+	@Test
+	public void testProperty() {
+		Path tempFile = Paths.get(ROOTPATH.toString(), "//config.info");
+		Tab testProp = testTree.newTab("testProp");
+		Project testProject = testTree.newProject("testProject1", testProp);
+		Item testItem = testTree.newItem("testitem.info", tempFile, testProject);
+		testTree.changeProperty(testItem, "desc", "testing");
+		assertEquals("{desc=testing}", testTree.getProperties(testItem).toString());
+	}
+	
+	@Test
+	public void testGFile() {
+		Tab testTab = testTree.newTab("testGFile");
+		testTab.changeName("newTestName");
+		assertEquals("newTestName", testTab.getName());
+	}
+	
+	
 }
